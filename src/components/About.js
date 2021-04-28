13eb6d573e725a8c283fe4d1ddaac38bfca82df9
Silambarasu <flytoasmano@gmail.com>
2021-04-28 16:15:26 +0530

    About me update - 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import photo from "../imag.jpg"
 
 function About() {
     return (
         <div id="aboutPage" className="aboutPage">
             <div className="main-about">
                 <h1>About Me:</h1>
+                <img className="photo" src={photo} alt="photo...." /> 
                 <div className="text1">
-                    <p>For several years now, I have focused my career path and academic pursuits on learning and developing those all-important skills necessary to be great in this type of role. 
+                    <p>For several years now, I have focused my career path and academic pursuits on learning and developing those all-important skills necessary to be great in full stack development. 
                         As a personal mission, I strive to empower and improve other's outlooks and circumstances.
                         I believe that being genuine and nurturing, along with professional and driven, is an essential combination for people in my field. 
                         I possess programming, software development and data analysis skills. 
