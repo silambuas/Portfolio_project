d9e4745f07a2703d8b5c74edfd4cdb27d619aa20
Silambarasu <flytoasmano@gmail.com>
2021-04-30 10:55:47 +0530

    Mistake of nav in aboutme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark" style={{position: "fixed", width: "100vw"}}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container">
       <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="Logo...." /> </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
